c84900edb8c4264f7ef8af895c8bbe620094b7c9
fryck <fankestuwar@gmail.com>
2020-10-12 19:50:42 -0500

    Agregue println en master

--- src/Ejemplo/Principal.java
+++ src/Ejemplo/Principal.java
@@ -3,8 +3,10 @@ package Ejemplo;
 public class Principal {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		// TODO Auto-generated method status
+		
+		System.out.println("Hola master");
+		
 	}
 
 }
